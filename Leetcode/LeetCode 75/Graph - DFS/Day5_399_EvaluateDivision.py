7552383ad9caead6c425866c15b2c646d6376a22
Nandivardhan Reddy <nandivardhanreddybhumireddy@gmail.com>
2025-01-20 06:31:42 -0600

    Medium

--- Leetcode/LeetCode 75/Graph - DFS/Day5_399_EvaluateDivision.py
+++ Leetcode/LeetCode 75/Graph - DFS/Day5_399_EvaluateDivision.py
@@ -35,6 +35,7 @@ sol=Solution()
 equations = [["a","b"],["b","c"]]
 values = [2.0,3.0]
 queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+print(sol.calcEquation(equations,values,queries))
 
 '''
 Complexity Analysis:

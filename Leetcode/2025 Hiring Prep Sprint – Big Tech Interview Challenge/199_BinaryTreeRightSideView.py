084a2a009ead21e2232f76c8f89bda135fb5d29c
Nandivardhan Reddy <nandivardhanreddybhumireddy@gmail.com>
2025-03-17 21:48:52 -0500

    Easy

---
+++ Leetcode/2025 Hiring Prep Sprint – Big Tech Interview Challenge/199_BinaryTreeRightSideView.py
@@ -0,0 +1,25 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root):
+        queue = deque([])
+        queue.append((root,0))
+        d={}
+        while queue:
+            node, level = queue.popleft()
+            if node is not None:
+                d[level] = node.val
+                if(node.left is not None):
+                    queue.append((node.left,level+1))
+                if node.right is not None:
+                    queue.append((node.right,level+1))
+        res = list(d.values())
+        return res
+
+root = [1,2,3,None,5,None,4]
